da6eb37a86eaaf264d7a32f67f4eafc6f4a249ca
maktempa <maktempgma@gmail.com>
2019-08-16 22:30:01 +0300

    changeset for friends.ex

--- backend/lib/backend/friendship.ex
+++ backend/lib/backend/friendship.ex
@@ -1,9 +1,14 @@
-defmodule Friendship do
+
   use Backend.Model
 
   schema "friendships" do
     field :active, :boolean, default: false
     belongs_to :requester_user, Backend.User
     belongs_to :respondent_user, Backend.User
+  end
+  def chan],eset(struct, params \\%{}) do
+    struct
+    |> cast(params, [:active, :requester_user, :respondent_user])
+    |> validate_required([:active], opts \\ [])
   end
 end
